7eb3b4ea40f4ca7e98e77a35267fe6e03d47194a
Skander Jabouzi <jabouzi@gmail.com>
2013-09-07 14:21:04 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@16 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- lateStaticBinding.php
+++ lateStaticBinding.php
@@ -1,5 +1,48 @@
 <?php
 
+abstract class Mammifere
+{
+    private $espece;
+    
+    public function __construct()
+    {
+        $this->espece = static::getEspece();
+    }
+    
+    public static function create()
+    {
+        return new static();
+    }
+    
+    public static function getEspece()
+    {
+        return 'mammifere';
+    }
+}
+
+class Mumain extends Mammifere 
+{
+    
+}
+
+class Animal extends Mammifere
+{
+    public static function getEspece()
+    {
+        return 'animal';
+    }
+}
+
+class Chien extends Animal
+{
+    
+}
+
+var_dump(Mumain::create());
+var_dump(Animal::create());
+var_dump(Chien::create());
+
+
 class A {
     public static function qui() {
         echo __CLASS__;
