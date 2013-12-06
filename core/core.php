37705af3dcc131624e0ac696d4ee2bdae54a50e8
Skander Jabouzi <jabouzi@gmail.com>
2013-12-06 03:28:56 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@25 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- core/core.php
+++ core/core.php
@@ -5,10 +5,11 @@ function get_controller_params($url_params)
     $core = array();
     if (!isset($url_params['u'])) return $core;
     $url_params = mysql_escape_string($url_params['u']);
-    $params = explode('/',$url_params);
-    $core['lang'] = $params[0];
-    $core['controller'] = ucfirst(strtolower($params[1]));
-    $core['method'] = $params[2];
-    $core['args'] = array_slice($params, 3);
+    $params = explode('/',trim($url_params,'/'));
+    var_dump($params);
+    if (isset($params[0])) $core['lang'] = $params[0];
+    else if (isset($params[1])) $core['controller'] = ucfirst(strtolower($params[1]));
+    else if (isset($params[2])) $core['method'] = $params[2];
+    else if (isset($params[3])) $core['args'] = array_slice($params, 3);
     return $core;
 }
--- index.php
+++ index.php
@@ -7,19 +7,30 @@ require_once('core/autoload.php');
 //require 'includes/exceptions.php';
 
 session_start();
-//var_dump($_GET);
+var_dump($_GET);
 $params = get_controller_params($_GET);
-//var_dump($params);
+var_dump($params);
 if (count($params))
 {
-    $obj = new $params['controller']();
-    if (is_callable(array($obj, $params['method'])))
+    if (isset($params['controller']))
     {
-        echo call_user_func_array(array($obj, $params['method']), $params['args']);
+        $obj = new $params['controller']();
+        if (is_callable(array($obj, $params['method'])))
+        {
+            echo call_user_func_array(array($obj, $params['method']), $params['args']);
+        }
+        else
+        {
+            echo 'Errorz';
+        }
     }
     else
     {
-        echo 'Errorz';
+        $obj = new defaults();
+        if (is_callable(array($obj, 'index')))
+        {
+            echo call_user_func_array(array($obj, 'index'), array());
+        }
     }
 }
 else
