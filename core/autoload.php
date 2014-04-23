212bba93ff726e1dd3740b64b0acb3152c881885
Skander Jabouzi <jabouzi@gmail.com>
2014-04-23 10:23:16 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@62 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- core/autoload.php
+++ core/autoload.php
@@ -4,21 +4,21 @@ class autoload
     public static function config_autoload($class)
     {
         $class = strtolower($class);
-        $path = $_SERVER['DOCUMENT_ROOT'] . "app/config/{$class}.php";
+        $path = APPPATH . "app/config/{$class}.php";
         if (is_readable($path)) require $path;
     }
     
     public static function core_autoload($class)
     {
         $class = strtolower($class);
-        $path = $_SERVER['DOCUMENT_ROOT'] . "core/{$class}.php";
+        $path = APPPATH . "core/{$class}.php";
         if (is_readable($path)) require $path;
     }
     
     public static function lib_autoload($class)
     {
         $class = strtolower($class);
-        $path = $_SERVER['DOCUMENT_ROOT'] . "app/lib/{$class}.php";
+        $path = APPPATH . "app/lib/{$class}.php";
         var_dump($path);
         if (is_readable($path)) require $path;
     }
@@ -26,28 +26,28 @@ class autoload
     public static function controller_autoload($class)
     {
         $class = strtolower($class);
-        $path = $_SERVER['DOCUMENT_ROOT'] . "app/controller/{$class}.php";
+        $path = APPPATH . "app/controller/{$class}.php";
         if (is_readable($path)) require $path;
     }
     
     public static function model_autoload($class)
     {
         $class = strtolower($class);
-        $path = $_SERVER['DOCUMENT_ROOT'] . "app/model/{$class}.php";
+        $path = APPPATH . "app/model/{$class}.php";
         if (is_readable($path)) require $path;
     }
     
     public static function class_autoload($class)
     {
         $class = strtolower($class);
-        $path = $_SERVER['DOCUMENT_ROOT'] . "app/class/{$class}.php";
+        $path = APPPATH . "app/class/{$class}.php";
         if (is_readable($path)) require $path;
     }
     
     public static function dao_autoload($class)
     {
         $class = strtolower($class);
-        $path = $_SERVER['DOCUMENT_ROOT'] . "app/dao/{$class}.php";
+        $path = APPPATH . "app/dao/{$class}.php";
         if (is_readable($path)) require $path;
     }
 }
