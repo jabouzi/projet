7dad68077ded639a3b71a71e991cc628617545f8
Skander Jabouzi <jabouzi@gmail.com>
2014-04-23 10:25:19 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@66 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-//ini_set('display_errors',1);
-//ini_set('display_startup_errors',1);
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
 $slash = '';
 if (substr($_SERVER['DOCUMENT_ROOT'], -1) != '/') $slash = '/';
 define('APPPATH', $_SERVER['DOCUMENT_ROOT'].$slash);
