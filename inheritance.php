490769c3e505ad453c99a07d8c2d4ab7af5cdb2d
Skander Jabouzi <jabouzi@gmail.com>
2014-04-24 10:11:11 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@81 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- inheritance.php
+++ inheritance.php
@@ -85,7 +85,7 @@ class Televesion extends Product {
     
     public function getName()
     {
-		return parent->productName;
+		return parent::productName;
         return parent::getName().'_NEW';
     }
     
