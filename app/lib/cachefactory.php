3416e5cb8aba4c708571437fb99d51067c1e5105
Skander Jabouzi <jabouzi@gmail.com>
2014-04-26 10:23:44 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@101 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

---
+++ app/lib/cachefactory.php
@@ -0,0 +1,75 @@
+<?php //if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+class Cachefactory {
+
+	private $cache;
+
+	private $cache_conf = array(
+					'host'		=> '127.0.0.1',
+					'port'		=> 11211,
+					'weight'	=> 1
+				);
+
+	function __construct()
+	{
+		if ( extension_loaded('memcached')) $this->cache = new Memcached();
+		else $this->cache = new Memcache();
+		$this->connect($this->cache_conf['host'], $this->cache_conf['port']);
+	}
+
+	public function get($id)
+	{
+		$data = $this->cache->get($id);
+
+		return (is_array($data)) ? $data[0] : FALSE;
+	}
+
+	public function save($id, $data, $ttl = 0)
+	{
+		return $this->cache->set($id, array($data, time(), $ttl), $ttl);
+	}
+
+	public function delete($id)
+	{
+		return $this->cache->delete($id);
+	}
+
+	public function clean()
+	{
+		return $this->cache->flush();
+	}
+
+	public function cache_info($type = NULL)
+	{
+		return $this->cache->getStats();
+	}
+
+	public function get_metadata($id)
+	{
+		$stored = $this->cache->get($id);
+
+		if (count($stored) !== 3)
+		{
+			return FALSE;
+		}
+
+		list($data, $time, $ttl) = $stored;
+
+		return array(
+			'expire'	=> $time + $ttl,
+			'mtime'		=> $time,
+			'data'		=> $data
+		);
+	}
+	
+	public function connect($host , $port){ 
+		//$servers = $this->cache->getServerList(); 
+		//if(is_array($servers)) { 
+			//foreach ($servers as $server) if($server['host'] == $host and $server['port'] == $port) return true; 
+		//} 
+		return $this->cache->addServer($host , $port; 
+	} 
+
+}
+
