03f4a9c3135b01617685a5450721a30d0409ba45
Skander Jabouzi <jabouzi@gmail.com>
2014-04-27 02:00:21 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@130 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- app/controller/login.php
+++ app/controller/login.php
@@ -5,7 +5,7 @@ class Login extends Controller
 	private $user;
 	private $userdao;
 	private $encrypt;
-	
+
     function __construct()
     {
         //$this->user_mdl = new User_model;
@@ -13,15 +13,16 @@ class Login extends Controller
         $this->userdao = new userprofiledao();
         $this->encrypt = new encryption();
     }
-    
+
     public function index($message = null)
     {
-		$data['message'] = $message;
+		if (is_array($message)) $data = $message;
+		else $data['message'] = $message;
 		view::load_view('default/standard/header');
 		view::load_view('default/login/form', $data);
 		view::load_view('default/standard/footer');
     }
-    
+
     public function process()
     {
 		if (isempty($_POST['email'])) $this->index('login.email.empty');
@@ -31,19 +32,19 @@ class Login extends Controller
 			$this->check_login($_POST['email'], $_POST['password']);
 		}
 	}
-	
+
 	public function logout()
 	{
 		unset($_SESSION['user_session']);
 		redirect('login');
 	}
-	
+
 	private function check_login($email, $password)
 	{
 		$this->user = $this->userdao->select_user($email);
 		if (!$this->user) $this->index('login.failed');
-		else if ($this->encrypt->decrypt($this->user->get_password()) == $password) $this->index('login.failed');
-		else if (!$this->user->get_status()) $this->index('account.nonactive');
+		else if ($this->encrypt->decrypt($this->user->get_password()) == $password) $this->index(array('message' => 'login.failed', 'email' => $email, 'password' => $password));
+		else if (!$this->user->get_status()) $this->index(array('message' => 'login.account.nonactive', 'email' => $email, 'password' => $password));
 		else redirect();
 	}
 }
