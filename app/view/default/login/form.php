8eaeaefdbf4a2cba881905ba0ab866545fa38504
Skander Jabouzi <jabouzi@gmail.com>
2014-04-27 00:53:09 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@120 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- app/view/default/login/form.php
+++ app/view/default/login/form.php
@@ -4,7 +4,7 @@
 		<ul class="error"><li><?php echo $message; ?></li></ul>
 	<?php endif; ?>
 	<form action="/<?php echo get_site_lang(); ?>/login/process" method="post">
-		<div class="row"><label for="username">Username:</label><input type="text" name="username" id="username" /></div>
+		<div class="row"><label for="email">Username:</label><input type="text" name="email" id="email" /></div>
 		<div class="row"><label for="password">Password:</label><input type="password" name="password" id="password" /></div>
 		<div class="row"><label for="submit"> </label><input id="submit" type="submit" value="login" class="submitbutton" /></div>
 	</form>
