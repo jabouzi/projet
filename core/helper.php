041c363ba0e02042a8d9a7f43dc94fa2310471c2
Skander Jabouzi <jabouzi@gmail.com>
2014-04-27 01:29:12 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@128 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- core/helper.php
+++ core/helper.php
@@ -6,7 +6,43 @@ function ucname($string) {
 	foreach (array('-', '\'') as $delimiter) {
 	  if (strpos($string, $delimiter)!==false) {
 		$string =implode($delimiter, array_map('ucfirst', explode($delimiter, $string)));
-	 , 302);
+	  }
+	}
+	return $string;
+}
+
+function user_agent()
+{
+	$user_agent = ( ! isset($_SERVER['HTTP_USER_AGENT'])) ? FALSE : $_SERVER['HTTP_USER_AGENT'];
+	return $user_agent;
+}
+
+function ip_address()
+{
+	return $_SERVER['REMOTE_ADDR'];
+}
+
+function get_site_lang()
+{
+	require(APPPATH.'/app/config/config.php');
+	$params = explode('/',trim($_GET['u'],'/'));
+	if (is_valid_site_lang()) return $params[0];
+	return $config['lang'];
+}
+
+function is_valid_site_lang()
+{
+	require(APPPATH.'/app/config/config.php');
+	$params = explode('/',trim($_GET['u'],'/'));
+	if (isset($params[0]) and in_array($params[0], $config['site_languages'])) return true;
+	return false;
+}
+
+function redirect($uri = '', $method = 'location', $http_response_code = 302)
+{
+	if ( ! preg_match('#^https?://#i', $uri))
+	{
+		header("Location: /".get_site_lang()."/".trim($uri,'/'), TRUE, 302);
 	}
 	else
 	{
