e27275bf3d33cac528f4a50dd06afddbeb5d34ca
Skander Jabouzi <jabouzi@gmail.com>
2014-04-28 10:18:07 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@147 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- app/controller/application.php
+++ app/controller/application.php
@@ -15,6 +15,7 @@ class Application extends Controller
 
 	public function index($message = null)
 	{
+		var_dump($_SESSION);
 		view::load_view('default/standard/header');
 		view::load_view('default/index/welcome');
 		view::load_view('default/standard/footer');
