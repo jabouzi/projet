f36cb90f3aac53b3e002a83ee3711e29d004bc4c
Skander Jabouzi <jabouzi@gmail.com>
2014-04-28 10:13:47 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@146 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

---
+++ app/controller/application.php
@@ -0,0 +1,22 @@
+<?php
+
+class Application extends Controller
+{
+	private $user;
+	private $userdao;
+	private $admin;
+	private $admindao;
+
+	function __construct()
+	{
+		//$this->user = new useradmin();
+		//$this->userdao = new useradmindao();
+	}
+
+	public function index($message = null)
+	{
+		view::load_view('default/standard/header');
+		view::load_view('default/index/welcome');
+		view::load_view('default/standard/footer');
+	}
+}
--- app/controller/login.php
+++ app/controller/login.php
@@ -6,25 +6,24 @@ class Login extends Controller
 	private $userdao;
 	private $encrypt;
 
-    function __construct()
-    {
-        //$this->user_mdl = new User_model;
-        $this->user = new useradmin();
-        $this->userdao = new useradmindao();
-        $this->encrypt = new encryption();
-    }
+	function __construct()
+	{
+		$this->user = new useradmin();
+		$this->userdao = new useradmindao();
+		$this->encrypt = new encryption();
+	}
 
-    public function index($message = null)
-    {
+	public function index($message = null)
+	{
 		if (is_array($message)) $data = $message;
 		else $data['message'] = $message;
 		view::load_view('default/standard/header');
 		view::load_view('default/login/form', $data);
 		view::load_view('default/standard/footer');
-    }
+	}
 
-    public function process()
-    {
+	public function process()
+	{
 		if (isempty($_POST['email'])) $this->index('login.email.empty');
 		else if (isempty($_POST['password'])) $this->index('login.password.empty');
 		else
@@ -45,6 +44,9 @@ class Login extends Controller
 		if (!$this->user) $this->index('login.failed');
 		else if (!$this->user->get_status()) $this->index(array('message' => 'login.account.nonactive', 'email' => $email, 'password' => $password));
 		else if ($this->encrypt->decrypt($this->user->get_password()) != $password) $this->index(array('message' => 'login.failed', 'email' => $email, 'password' => $password));
-		else redirect();
+		else {
+			$_SESSION['user'] = $this->user;
+			redirect('application');
+		}
 	}
 }
