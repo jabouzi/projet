c883a5fe85333fca6469fb01c457b4064394658c
Skander Jabouzi <jabouzi@gmail.com>
2014-04-29 09:42:00 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@151 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- app/controller/application.php
+++ app/controller/application.php
@@ -10,13 +10,14 @@ class Application extends Controller
 	function __construct()
 	{
 		var_dump($_SESSION);
+		if (!is_logged()) redirect('login');
 		//$this->user = new useradmin();
 		//$this->userdao = new useradmindao();
 	}
 
 	public function index($message = null)
 	{
-		if (!is_logged()) redirect(login);
+		
 		view::load_view('default/standard/header');
 		view::load_view('default/index/welcome');
 		view::load_view('default/standard/footer');
