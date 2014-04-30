b32480b46cb215975ca48eaee975620e0c95e1f1
Skander Jabouzi <jabouzi@gmail.com>
2014-04-30 22:12:10 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@193 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- app/view/default/contacts/edit.php
+++ app/view/default/contacts/edit.php
@@ -1,5 +1,5 @@
-<h2>Edit Contact</h2>
-<form action="/contacts/processedit" method="post" id="editform">
+<h2>Edit Account</h2>
+<form action="/<?php echo get_site_lang(); ?>/application/processedit" method="post" id="editform">
     <input type="hidden" name="id" value="3" /><div class="row"><label for="firstname">First Name:</label><input name="firstname" id="firstname" value="skander" /></div><div class="row"><label for="middlename">Middle Name:</label><input name="middlename" id="middlename" value="" /></div><div class="row"><label for="lastname">Last Name:</label><input name="lastname" id="lastname" value="Jabouzi" /></div>
     
         
