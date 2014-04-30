cad8a6711d15966b9f42660738d302d4a804e231
Skander Jabouzi <jabouzi@gmail.com>
2014-04-30 23:04:43 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@209 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- app/view/default/login/form.php
+++ app/view/default/login/form.php
@@ -1,7 +1,7 @@
 <div id="loginbox">
 	<h1>Login</h1>
 	<form action="/<?php echo get_site_lang(); ?>/login/process" method="post">
-		<div class="row error"><?php display_message(); ?></div>
+		<div class="row error"><label></label><?php display_message(); ?></div>
 		<div class="row"><label for="email">Username:</label><input type="text" name="email" id="email" value="<?php if (isset($email)) echo $email; ?>"/></div>
 		<div class="row"><label for="password">Password:</label><input type="password" name="password" id="password" value="<?php if (isset($password)) echo $password; ?>"/></div>
 		<div class="row"><label for="submit"> </label><input id="submit" type="submit" value="login" class="submitbutton" /></div>
