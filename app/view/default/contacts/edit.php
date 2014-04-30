1e8faee0ee0aa268af2a44b2a97c782485808279
Skander Jabouzi <jabouzi@gmail.com>
2014-04-30 22:09:24 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@190 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- app/view/default/contacts/edit.php
+++ app/view/default/contacts/edit.php
@@ -1,6 +1,6 @@
 <h2>Edit Contact</h2>
 <form action="/contacts/processedit" method="post" id="editform">
-    <input type="hidden" name="id" value="3" /><div class="row"><label for="firstname">First Name:</label><input name="firstname" id="firstname" value="skander" /></div><div class="row"><label for="middlename">Middle Name:</label><input name="middlename" id="middlename" value="" /></div><div class="row"><label for="lastname">Last Name:</label><input name="lastname" id="lastname" value="Jabouzi" /></div>    <hr />
+    <input type="hidden" name="id" value="3" /><div class="row"><label for="firstname">First Name:</label><input name="firstname" id="firstname" value="skander" /></div><div class="row"><label for="middlename">Middle Name:</label><input name="middlename" id="middlename" value="" /></div><div class="row"><label for="lastname">Last Name:</label><input name="lastname" id="lastname" value="Jabouzi" /></div>
     
         <div class="contactgrouping">
         <div class="row"><label>Grouping:</label>
@@ -11,10 +11,7 @@
           <select name="type[1][methodtype][]"><option value="" selected="selected">-Choose One-</option><option value="organization" >Organization</option><option value="title" >Title</option><option value="email" >Email</option><option value="website" >Website</option><option value="address" >Complete Address</option><option value="telephone" >Telephone</option><option value="mobilephone" >Mobile Phone</option><option value="socialnetwork" >Social Network URL</option><option value="im" >IM Name</option></select><span class="methodboxvaluebox "><input name="type[1][methodvalue][]" value="" /><a href="#" class="addcontactmethod">Add More Info</a></span></div>
             
         </div>
-         
-        <a href="#" class="addcontactgrouping">Add Another Grouping</a>    </div>
-    
-    <hr id="lastclone" />
+
     <div><label for="submit"> </label>
          <input id="submit" type="submit" value="Edit Contact" class="submitbutton" />
     </div>
