69e6ef772d2112d9e8b9139bfddfacca7a0961b8
Skander Jabouzi <jabouzi@gmail.com>
2014-04-30 23:08:48 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@213 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- app/view/default/contacts/browse.php
+++ app/view/default/contacts/browse.php
@@ -3,8 +3,8 @@
 <?php
 	foreach ($users as $user) {
 		var_dump($user);
-		echo '<a href="/'.get_site_lang().'/application/edit/' . $user->get_first_name() . '">';
-		echo $user['user_first_name'] . " " . $user->get_last_name();
+		echo '<a href="/'.get_site_lang().'/application/edit/' . $user->get_user_name() . '">';
+		echo $user->get_first_name() . " " . $user->get_last_name();
 		echo '</a>';
 	}
 	echo '</div>';
