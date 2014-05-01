428acbac1b58765cf9fecc2403e1bdd225b53f65
Skander Jabouzi <jabouzi@gmail.com>
2014-05-01 01:31:45 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@226 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- app/view/default/contacts/edit.php
+++ app/view/default/contacts/edit.php
@@ -1,15 +1,24 @@
 <?php display_message(); ?>
 <h2>Edit Account</h2>
 <form action="/<?php echo get_site_lang(); ?>/application/processedit" method="post" id="editform">
-    <input type="hidden" name="id" value="3" /><div class="row"><label for="firstname">First Name:</label><input name="firstname" id="firstname" value="skander" /></div><div class="row"><label for="middlename">Middle Name:</label><input name="middlename" id="middlename" value="" /></div><div class="row"><label for="lastname">Last Name:</label><input name="lastname" id="lastname" value="Jabouzi" /></div>
-    
-        
-        <div>
-            <div class="row"><label>Info:</label>
-          <select name="type[1][methodtype][]"><option value="" selected="selected">-Choose One-</option><option value="organization" >Organization</option><option value="title" >Title</option><option value="email" >Email</option><option value="website" >Website</option><option value="address" >Complete Address</option><option value="telephone" >Telephone</option><option value="mobilephone" >Mobile Phone</option><option value="socialnetwork" >Social Network URL</option><option value="im" >IM Name</option></select><span class="methodboxvaluebox "><input name="type[1][methodvalue][]" value="" /><a href="#" class="addcontactmethod">Add More Info</a></span></div>
-            
-        </div>
-
+    <input type="hidden" name="id" value="3" />
+    <div class="row">
+		<label for="firstname">First Name:</label>
+		<input name="firstname" id="firstname" value="skander" />
+	</div>
+	<div class="row">
+		<label for="middlename">Middle Name:</label>
+		<input name="middlename" id="middlename" value="" />
+	</div>
+	<div class="row">
+		<label for="lastname">Last Name:</label>
+		<input name="lastname" id="lastname" value="Jabouzi" />
+	</div>
+	<div>
+	<div class="row">
+		<label>Info:</label>
+		<?php get_projects(); ?>
+	</div>
     <div><div class="row"><label for="submit"> </label>
          <input id="submit" type="submit" value="Edit Contact" class="submitbutton" />
     </div>
