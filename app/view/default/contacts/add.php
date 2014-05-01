fcdaa10f1fc04998e9dbc8edf946abfaff1c55a2
Skander Jabouzi <jabouzi@gmail.com>
2014-05-01 09:54:53 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@263 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- app/view/default/contacts/add.php
+++ app/view/default/contacts/add.php
@@ -1,30 +1,30 @@
 <?php display_message();?>
-<h2>Edit Account</h2>
+<h2>Add Account</h2>
 <form action="/<?php echo get_site_lang(); ?>/application/processadd" method="post" id="editform">
     <input type="hidden" name="id" value="3" />
     <div class="row">
 		<label for="user_name">User Name:</label>
-		<input type="text" name="user_name" id="user_name" value="<?php echo $user->get_user_name(); ?>" />
+		<input type="text" name="user_name" id="user_name" value="" />
 	</div>
     <div class="row">
 		<label for="email">Email:</label>
-		<input type="text" name="email" id="email" value="<?php echo $user->get_email(); ?>" />
+		<input type="text" name="email" id="email" value="" />
 	</div>
     <div class="row">
 		<label for="password">password:</label>
-		<input type="password" name="password" id="password" value="<?php echo $user->get_password(); ?>" />
+		<input type="password" name="password" id="password" value="" />
 	</div>
     <div class="row">
 		<label for="firs_tname">First Name:</label>
-		<input type="text" name="firs_tname" id="firs_tname" value="<?php echo $user->get_first_name(); ?>" />
+		<input type="text" name="firs_tname" id="firs_tname" value="" />
 	</div>
 	<div class="row">
 		<label for="las_tname">Last Name:</label>
-		<input type="text" name="las_tname" id="las_tname" value="<?php echo $user->get_last_name(); ?>" />
+		<input type="text" name="las_tname" id="las_tname" value="" />
 	</div>
 	<div class="row">
 		<label>Info:</label>
-		<?php get_projects($user->get_user_vhosts()); ?>
+		<?php get_projects(); ?>
 	</div>
     <div><div class="row"><label for="submit"> </label>
          <input id="submit" type="submit" value="Edit Contact" class="submitbutton" />
