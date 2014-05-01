8cc6435b687b0330fd1e67e917f0b80d7d8fb489
Skander Jabouzi <jabouzi@gmail.com>
2014-05-01 09:38:30 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@251 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- app/dao/userdatadao.php
+++ app/dao/userdatadao.php
@@ -129,7 +129,7 @@ class Userdatadao {
 		if (!count($result)) return false;
 		$query = "SELECT user_vhost FROM user_vhost WHERE user_name = :user_name";
 		$result2 = $this->db->query($query, $args);
-		$result[0]['user_vhost'] = $result2[0];
+		$result[0]['user_vhost'] = $result2;
 		$builder = new userdatabuilder($result[0]);
 		$builder->build();
 		$user = $builder->getUser();
