dd1fb3ed6172dab2cde7cf3b31fa527206442ddc
Skander Jabouzi <jabouzi@gmail.com>
2014-05-01 09:50:23 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@258 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- app/helper/functions.php
+++ app/helper/functions.php
@@ -5,6 +5,7 @@ function get_projects($user_projects)
 	var_dump($user_projects);
     $sClient = new SoapClient('http://svn.tgiprojects.com/wsdl/usvnws.wsdl', array('trace' => 1));
     $projects = $sClient->getlist();
+    var_dump($projects);
     echo '<select id="_projects" name="_projects" multiple="multiple"><option value="*">All</option>';
     foreach($projects as $project)
     {
