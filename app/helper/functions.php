a7a6d4d3291c093c8d845266c9660f406821d9aa
Skander Jabouzi <jabouzi@gmail.com>
2014-05-01 01:28:10 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@224 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- app/helper/functions.php
+++ app/helper/functions.php
@@ -4,13 +4,12 @@ function get_projects()
 {
     $sClient = new SoapClient('http://svn.tgiprojects.com/wsdl/usvnws.wsdl', array('trace' => 1));
     $projects = $sClient->getlist();
-    var_dump($projects);
-    //echo '<select id="projects" name="projects"><option value="*">All</option>';
-    //foreach($projects as $project)
-    //{
-        //$selected = '';
-        //if ($id == $project['code_c']) $selected = 'selected';
-        //echo '<option value="'.$project['code_c'].'" '.$selected.' >'.ucfirst(strtolower($project['nom_legal'])).'</option>';
-    //}
-    //echo '</select>';
+    echo '<select id="_projects" name="projects"><option value="*">All</option>';
+    foreach($projects as $project)
+    {
+        echo '<option value="'.$project['projects_sitestaging'].'">'.$project['projects_sitestaging']).'</option>';
+    }
+    echo '</select>
+    <span id="projects">
+	</span>';
 }
--- app/view/default/standard/header.php
+++ app/view/default/standard/header.php
@@ -1,10 +1,28 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
-<html xmlns="http://www.w3.org/1999/xhtml"> 
-<head> 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<link rel="stylesheet" type="text/css" href="/public/css/main.css" />
+	<link rel="stylesheet" type="text/css" href="/public/css/smoothness-1.8.13/jquery-ui-1.8.13.custom.css">
+	<link rel="stylesheet" type="text/css" href="/public/css/ui.dropdownchecklist.themeroller.css">
+	<script type="text/javascript" src="/public/js/jquery-1.6.1.min.js"></script>
+	<script type="text/javascript" src="/public/js/jquery-ui-1.8.13.custom.min.js"></script>
+	<script type="text/javascript" src="/public/js/ui.dropdownchecklist-1.4-min.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function() {
+			$returnS5 = $('#projects');
+			$("#_projects").dropdownchecklist({
+				onItemClick: function(checkbox, selector){
+					var justChecked = checkbox.prop("checked");
+					var checkCount = (justChecked) ? 1 : -1;
+					for( i = 0; i < selector.options.length; i++ ){
+						if ( selector.options[i].selected ) checkCount += 1;
+					}
+				}
+			});
+	   });
+	</script>
 	<title>Acme Company Contact Manager</title>
-    <script type="text/javascript" src="/public/js/jquery-1.3.2.min.js"></script>
 </head>
 <body>
-    
+
