a88f7ca5f5565b137297b8501910dd16425a7880
Skander Jabouzi <jabouzi@gmail.com>
2014-05-01 00:54:16 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@221 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

---
+++ app/helper/functions.php
@@ -0,0 +1,16 @@
+<?php
+
+function get_projects()
+{
+    $sClient = new SoapClient('http://http://svn.tgiprojects.com/wsdl/usvnws.wsdl', array('trace' => 1));
+    $projects = $sClient->getlist();
+    var_dump($projects);
+    //echo '<select id="projects" name="projects"><option value="*">All</option>';
+    //foreach($projects as $project)
+    //{
+        //$selected = '';
+        //if ($id == $project['code_c']) $selected = 'selected';
+        //echo '<option value="'.$project['code_c'].'" '.$selected.' >'.ucfirst(strtolower($project['nom_legal'])).'</option>';
+    //}
+    //echo '</select>';
+}
