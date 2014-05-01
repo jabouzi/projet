a75b7cebcb1afe00db9944c05202c916033a384a
Skander Jabouzi <jabouzi@gmail.com>
2014-05-01 09:17:22 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@242 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- app/view/default/standard/header.php
+++ app/view/default/standard/header.php
@@ -2,11 +2,11 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<?php/*<link rel="stylesheet" type="text/css" href="/public/css/smoothness-1.8.13/jquery-ui-1.8.13.custom.css">
+	<?/*<link rel="stylesheet" type="text/css" href="/public/css/smoothness-1.8.13/jquery-ui-1.8.13.custom.css">
 	<link rel="stylesheet" type="text/css" href="/public/css/ui.dropdownchecklist.themeroller.css">*/?>
 	<link rel="stylesheet" type="text/css" href="/public/css/main.css" />
 	<script type="text/javascript" src="/public/js/jquery-1.6.1.min.js"></script>
-	<?php/*<script type="text/javascript" src="/public/js/jquery-ui-1.8.13.custom.min.js"></script>
+	<?/*<script type="text/javascript" src="/public/js/jquery-ui-1.8.13.custom.min.js"></script>
 	<script type="text/javascript" src="/public/js/ui.dropdownchecklist-1.4-min.js"></script>
 	<script type="text/javascript">
 		$(document).ready(function() {
