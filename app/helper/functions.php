d8c26b6aa2c6a7caef06a20559963c37f5a0c5f8
Skander Jabouzi <jabouzi@gmail.com>
2014-05-01 09:48:24 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@256 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- app/helper/functions.php
+++ app/helper/functions.php
@@ -1,13 +1,15 @@
 <?php
 
-function get_projects()
+function get_projects($user_projects)
 {
     $sClient = new SoapClient('http://svn.tgiprojects.com/wsdl/usvnws.wsdl', array('trace' => 1));
     $projects = $sClient->getlist();
     echo '<select id="_projects" name="_projects" multiple="multiple"><option value="*">All</option>';
     foreach($projects as $project)
     {
-        echo '<option value="'.$project['projects_sitestaging'].'" selected>'.$project['projects_sitestaging'].'</option>';
+		$selected = '';
+		if (in_array($user_projects, $project['projects_sitestaging'])) $selected = 'selected';
+        echo '<option value="'.$project['projects_sitestaging'].'" '.$selected.'>'.$project['projects_sitestaging'].'</option>';
     }
     echo '</select>
     <span id="projects">
--- app/view/default/contacts/edit.php
+++ app/view/default/contacts/edit.php
@@ -3,20 +3,28 @@
 <form action="/<?php echo get_site_lang(); ?>/application/processedit" method="post" id="editform">
     <input type="hidden" name="id" value="3" />
     <div class="row">
-		<label for="firstname">First Name:</label>
-		<input name="firstname" id="firstname" value="skander" />
+		<label for="user_name">User Name:</label>
+		<input type="text" name="user_name" id="user_name" value="<?php echo $user->get_user_name(); ?>" />
+	</div>
+    <div class="row">
+		<label for="email">Email:</label>
+		<input type="text" name="email" id="email" value="<?php echo $user->get_email(); ?>" />
+	</div>
+    <div class="row">
+		<label for="password">password:</label>
+		<input type="password" name="password" id="password" value="<?php echo $user->get_password(); ?>" />
+	</div>
+    <div class="row">
+		<label for="firs_tname">First Name:</label>
+		<input type="text" name="firs_tname" id="firs_tname" value="<?php echo $user->get_first_name(); ?>" />
 	</div>
 	<div class="row">
-		<label for="middlename">Middle Name:</label>
-		<input name="middlename" id="middlename" value="" />
-	</div>
-	<div class="row">
-		<label for="lastname">Last Name:</label>
-		<input name="lastname" id="lastname" value="Jabouzi" />
+		<label for="las_tname">Last Name:</label>
+		<input type="text" name="las_tname" id="las_tname" value="<?php echo $user->get_last_name(); ?>" />
 	</div>
 	<div class="row">
 		<label>Info:</label>
-		<?php get_projects(); ?>
+		<?php get_projects($user->get_user_vhosts()); ?>
 	</div>
     <div><div class="row"><label for="submit"> </label>
          <input id="submit" type="submit" value="Edit Contact" class="submitbutton" />
