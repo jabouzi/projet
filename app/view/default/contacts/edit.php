7e289acf06914be2ddda64bc3aa513086e6860b6
Skander Jabouzi <jabouzi@gmail.com>
2014-05-02 10:03:00 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@287 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- app/view/default/contacts/edit.php
+++ app/view/default/contacts/edit.php
@@ -3,16 +3,15 @@
 <form action="/<?php echo get_site_lang(); ?>/application/delete" method="post" id="deleteform">    
 <div>
 	<div class="row"><label for="submit"> </label>
-         <input id="submit" type="submit" value="Delete Contact" class="deletebutton" />
+         <input id="delete" type="button" value="Delete Contact" class="deletebutton" />
     </div>
 	<div class="row"><label for="submit"> </label>
         <input type="hidden" name="user_name" id="user_name" value="<?php echo $user->get_user_name(); ?>"/> 
     </div>
 </div>
-
 </form>
+
 <form action="/<?php echo get_site_lang(); ?>/application/processedit" method="post" id="editform">
-    <input type="hidden" name="id" value="3" />
     <div class="row">
 		<label for="user_name">User Name:</label>
 		<input type="text" name="user_name" id="user_name" value="<?php echo $user->get_user_name(); ?>" readonly />
