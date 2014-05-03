e26c491dc80d76bfdb931657441e33db8db35ceb
Skander Jabouzi <jabouzi@gmail.com>
2014-05-03 13:57:04 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@397 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- app/helper/functions.php
+++ app/helper/functions.php
@@ -2,18 +2,19 @@
 
 function get_projects($user_projects = array())
 {
-    $sClient = new SoapClient('http://svn.tgiprojects.com/wsdl/usvnws.wsdl', array('trace' => 1));
-    $projects = $sClient->getlist();
-    $selected = '';
-    if (in_array('*', $user_projects)) $selected = 'selected'; 
-    echo '<select id="_projects" name="_projects[]" multiple="multiple"><option value="*" '.$selected.'>All</option>';
-    foreach($projects as $project)
-    {
+	$sClient = new SoapClient('http://svn.tgiprojects.com/wsdl/usvnws.wsdl', array('trace' => 1));
+	$projects = $sClient->getlist();
+	$selected = '';
+	if (in_array('*', $user_projects)) $selected = 'selected';
+	echo '<select id="_projects" name="_projects[]" multiple="multiple" data-validate="required">
+				<option value="*" '.$selected.'>All</option>';
+	foreach($projects as $project)
+	{
 		$selected = '';
 		if (in_array($project['projects_sitestaging'], $user_projects)) $selected = 'selected';
-        echo '<option value="'.$project['projects_sitestaging'].'" '.$selected.'>'.$project['projects_sitestaging'].'</option>';
-    }
-    echo '</select>
-    <span id="projects">
+		echo '<option value="'.$project['projects_sitestaging'].'" '.$selected.'>'.$project['projects_sitestaging'].'</option>';
+	}
+	echo '</select>
+	<span id="projects">
 	</span>';
 }
