33efba8131082a394fb32cbaec377b8bf5281120
Skander Jabouzi <jabouzi@gmail.com>
2014-05-03 11:37:25 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@334 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- public/js/vform.js
+++ public/js/vform.js
@@ -2,8 +2,8 @@ function validate_from(form_id)
 {
     var required = 0;
     $('input').removeClass('error_input');
-    $("#" + form_id).find('[data-validate]').each(function() {
-        required += validate_element($(this));
+    $("#" + form_id).find('[data-validate]').each(function(element) {
+        required += validate_element(element);
     });
         
     process_validated_form(required, form_id);
