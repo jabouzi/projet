cc529f57cb44fbbd7f0c9ad5e7988bbf604578bd
Skander Jabouzi <jabouzi@gmail.com>
2014-05-03 10:07:08 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@314 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- core/helper.php
+++ core/helper.php
@@ -91,7 +91,7 @@ function isadmin()
 
 function display_message()
 {
-	echo (isset($_SESSION['message'])) ? $_SESSION['message'] : '';
+	echo (isset($_SESSION['message'])) ? '<label>'.$_SESSION['message'].'</label>' : '';
 	unset($_SESSION['message']);
 }
 
