e372fa47e1f11f40943d8e93472949629f34e5be
Skander Jabouzi <jabouzi@gmail.com>
2014-05-04 14:14:10 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@448 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

---
+++ app/controller/admin.php
@@ -0,0 +1,99 @@
+<?php
+
+class Application extends Controller
+{
+	//private $user;
+	//private $userdao;
+	private $user_model;
+
+
+	function __construct()
+	{
+		if (!islogged()) redirect('login');
+		$this->user_model = new User_model();
+	}
+
+	public function index($message = null)
+	{
+		//menu decorator
+		$users = new useriterator($this->user_model->get_users());
+		view::load_view('default/standard/header');
+		view::load_view('default/standard/menu');
+		if ($users)
+		{
+			$data['users'] = $users;
+			view::load_view('default/contacts/userslist', $data);
+		}
+		else
+		{
+			view::load_view('default/index/welcome');
+		}
+		view::load_view('default/standard/footer');
+	}
+
+	public function add()
+	{
+		view::load_view('default/standard/header');
+		view::load_view('default/standard/menu');
+		view::load_view('default/contacts/add');
+		view::load_view('default/standard/footer');
+		unset($_SESSION['request']);
+	}
+
+	public function edit($user_name)
+	{
+		//username no change
+		$user = $this->user_model->get_user($user_name);
+		$data['user'] = $user;
+		view::load_view('default/standard/header');
+		view::load_view('default/standard/menu');
+		view::load_view('default/contacts/edit', $data);
+		view::load_view('default/standard/footer');
+		unset($_SESSION['request']);
+	}
+
+	public function delete()
+	{
+		$this->user_model->delete_user($_POST['user_name']);
+		$_SESSION['message'] = 'account.user_delete';
+		redirect('/');
+	}
+
+	public function processadd()
+	{
+		if ($this->user_model->user_email_exists($_POST['user_email']))
+		{
+			$_SESSION['request'] = $_POST;
+			$_SESSION['message'] = 'account.email.exists';
+			redirect('application/add');
+		}
+		else if ($this->user_model->user_name_exists($_POST['user_name']))
+		{
+			$_SESSION['request'] = $_POST;
+			$_SESSION['message'] = 'account.user_name.exists';
+			redirect('application/add');
+		}
+		else
+		{
+			$this->user_model->add_user($_POST);
+			$_SESSION['message'] = 'account.user_added';
+			redirect('/');
+		}
+	}
+
+	public function processedit()
+	{
+		if ($this->user_model->user_email_exists($_POST['user_email'], $_POST['user_name']))
+		{
+			$_SESSION['request'] = $_POST;
+			$_SESSION['message'] = 'account.email.exists';
+			redirect('application/edit/'.$_POST['user_name']);
+		}
+		else
+		{
+			$this->user_model->update_user($_POST);
+			$_SESSION['message'] = 'account.user_updated';
+			redirect('application/edit/'.$_POST['user_name']);
+		}
+	}
+}
