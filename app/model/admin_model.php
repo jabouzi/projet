1f1d1febd196c4b6bd9e9b49dd2a67d97d0c45b5
Skander Jabouzi <jabouzi@gmail.com>
2014-05-04 14:13:43 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@447 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

---
+++ app/model/admin_model.php
@@ -0,0 +1,75 @@
+<?php
+
+class User_model extends Model
+{
+	private $account;
+	private $accountdao;
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->account = new userdata();
+		$this->accountdao = new userdatadao();
+	}
+
+	public function add_user($user)
+	{
+		$user['user_vhost'] = adjust_vhosts($user['user_vhost']);
+		$builder = new userdatabuilder($user);
+		$builder->build();
+		$user = $builder->getUser();
+		$this->accountdao->insert_info($user);
+		$this->accountdao->insert_vhosts($user);
+	}
+
+	public function update_user($user)
+	{
+		$user['user_vhost'] = adjust_vhosts($user['user_vhost']);
+		$builder = new userdatabuilder($user);
+		$builder->build();
+		$user = $builder->getUser();
+		$this->accountdao->update_info($user);
+		$this->accountdao->update_vhosts($user);
+	}
+	
+	public function delete_user($user_name)
+	{
+		$this->accountdao->delete_info($user_name);
+	}
+
+	public function get_user($user_name)
+	{
+		return $this->accountdao->select_account($user_name);
+	}
+
+	public function get_users()
+	{
+		return $this->accountdao->select_all();
+	}
+
+	public function user_email_exists($user_email, $user_name = '')
+	{
+		$and = '';
+		$args = array(
+			':user_email' => $user_email
+		);
+		if ($user_name != '')
+		{
+			$args[':user_name'] = $user_name;
+			$and = ' AND user_name != :user_name';
+		}
+		$query = "SELECT count(*) as count FROM user_info WHERE user_email = :user_email {$and} ";
+		$count = $this->db->query($query, $args);
+		return intval($count[0]['count']);
+	}
+
+	public function user_name_exists($user_name)
+	{
+		$args = array(
+			':user_name' => $user_name
+		);
+		$query = "SELECT count(*) as count FROM user_info WHERE user_name = :user_name";
+		$count = $this->db->query($query, $args);
+		return intval($count[0]['count']);
+	}
+}
