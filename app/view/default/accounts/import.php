2c8eaa56b1fd69d4640bb2e705593b94b97ad4ee
Skander Jabouzi <jabouzi@gmail.com>
2014-05-05 09:23:49 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@526 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- app/view/default/accounts/import.php
+++ app/view/default/accounts/import.php
@@ -3,7 +3,7 @@
 <p>
     Upload a .csv file or an xml file or an excel file.
 </p>
-<form action="/<?php echo get_site_lang(); ?>/accounts/processimport" method="post" enctype="multipart/form-data">
+<form action="/<?php echo get_site_lang(); ?>/application/processimport" method="post" enctype="multipart/form-data">
     <input type="hidden" name="importtype" value="outlook" />
     <div class="row">
         <label for="contactsfile">Accounts File:</label><input type="file" id="contactsfile" name="contactsfile" />
