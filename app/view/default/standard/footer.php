dcda916c4457f5c61ccb28839fd1f2179d1e3856
Skander Jabouzi <jabouzi@gmail.com>
2014-05-05 09:18:05 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@522 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- app/view/default/standard/footer.php
+++ app/view/default/standard/footer.php
@@ -1,5 +1,5 @@
 </div>
-    <div id="footer">© TGI <?php echo date('Y'); ?>
+    <div id="footer"><a href="#">© TGI <?php echo date('Y'); ?></a>
 | <a href="mailto:skander.jabouzi@tonikgroupimage.com">Contact Help</a></div>
 </body>
 </html>
