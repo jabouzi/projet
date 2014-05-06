c602a95c4e663534c9819c984f963f469002a80f
Skander Jabouzi <jabouzi@gmail.com>
2014-05-06 10:35:52 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@581 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- app/lib/csvimportadapter.php
+++ app/lib/csvimportadapter.php
@@ -9,21 +9,17 @@ class Csvimportadapter
 
 	}
 
-	public function import($json)
+	public function import($csv)
 	{
+		$users = array();
 		$row = 1;
-if (($handle = fopen("test.csv", "r")) !== FALSE) {
-    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        $num = count($data);
-        echo "<p> $num fields in line $row: <br /></p>\n";
-        $row++;
-        for ($c=0; $c < $num; $c++) {
-            echo $data[$c] . "<br />\n";
-        }
-    }
-    fclose($handle);
-}
-		$users = json_decode($json, true);
+		if (($handle = fopen($csv, "r")) !== FALSE) {
+			while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
+				$data['user_vhost'] = explde(',', $data['user_vhost']);
+				$users[] = $data;
+			}
+			fclose($handle);
+		}
 		$this->userimport = new userimport();
 		$this->userimport->import($users);
 	}
