ed9eb87cfad37fc74f322061ec8608d26984c43f
Skander Jabouzi <jabouzi@gmail.com>
2014-05-06 09:42:12 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@552 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- app/lib/jsonimportadapter.php
+++ app/lib/jsonimportadapter.php
@@ -6,6 +6,8 @@ class Jsonimportadapter
 	
 	function __construct($json)
 	{
+		$test = array(array('user_name' => 'skander'), array('user_name' => 'jabouzi'));
+		var_dump(json_encode($test));
 		var_dump($json);
 		$users = json_decode($json);
 		var_dump($users);
