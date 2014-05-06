060714904564d9fe1c01f9508c36ab2f9c6bbad2
Skander Jabouzi <jabouzi@gmail.com>
2014-05-06 12:37:05 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@590 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- app/lib/csvimportadapter.php
+++ app/lib/csvimportadapter.php
@@ -11,22 +11,24 @@ class Csvimportadapter
 
 	public function import($csv)
 	{
-		var_dump($csv);
 		$users = array();
 		$params = array('user_name', 'user_password', 'user_first_name', 'user_last_name', 'user_email');
 		$index = 0;
 		$alldata = str_getcsv($csv, "\n");
 		foreach($alldata as &$data)
 		{
-			$data = str_getcsv($data, ";");
-			$users['user_vhost'] = explode(',', $data[5]);
-			foreach($params as $key => $value)
+			if ($index)
 			{
-				$users[$index][$value] = $data[$key];
+				$data = str_getcsv($data, ";");
+				$users['user_vhost'] = explode(',', $data[5]);
+				foreach($params as $key => $value)
+				{
+					$users[$index][$value] = $data[$key];
+				}
 			}
-			var_dump($data);
 			$index++;
 		}
+		var_dump($users);
 		exit;
 		$this->userimport = new userimport();
 		$this->userimport->import($users);
