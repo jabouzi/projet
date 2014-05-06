c97bd235f6fd0bbc023571551161f3d04ab2116b
Skander Jabouzi <jabouzi@gmail.com>
2014-05-06 10:46:28 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@587 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- app/lib/csvimportadapter.php
+++ app/lib/csvimportadapter.php
@@ -11,6 +11,7 @@ class Csvimportadapter
 
 	public function import($csv)
 	{
+		var_dump($csv);
 		$users = array();
 		$params = array('user_name', 'user_password', 'user_first_name', 'user_last_name', 'user_email');
 		$row = 0;
