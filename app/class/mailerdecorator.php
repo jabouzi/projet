8cee818b4892e7d0038153e5ac89f7088ac8cfdf
Skander Jabouzi <jabouzi@gmail.com>
2014-05-07 23:20:17 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@664 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- app/class/mailerdecorator.php
+++ app/class/mailerdecorator.php
@@ -4,12 +4,12 @@ class Mailerdecorator
 {
 	private $message;
 	private $mailer;
-	
+
 	function __construct()
 	{
-		
+
 	}
-	
+
 	public function decorateuser($user, $email)
 	{
 		$this->message = sprintf($email,
@@ -24,10 +24,8 @@ class Mailerdecorator
 				$user['user_name'],
 				$user['user_password']
 			);
-			
-	var_dump($this->message);
 	}
-	
+
 	public function decorateadmin($user, $email)
 	{
 		$this->message = sprintf($email,
@@ -41,7 +39,7 @@ class Mailerdecorator
 				$user['password']
 			);
 	}
-	
+
 	public function sendusermail($user)
 	{
 		try {
@@ -49,9 +47,9 @@ class Mailerdecorator
 			$this->mailer->setFrom("TGI", "contact@tonikgrupimage.com");
 			$this->mailer->addRecipient($user['user_first_name'].', '.$user['user_last_name'], $user['user_email']);
 			$this->mailer->fillSubject("Tonik Stagin website");
-			$this->mailer->fillMessage($this->message);			
+			$this->mailer->fillMessage($this->message);
 			$this->mailer->addRecipient("Skander Jabouzi", "skander.jabouzi@tonikgroupimage.com");
-			
+
 			// now we send it!
 			$this->mailer->send();
 		} catch (Exception $e) {
@@ -59,7 +57,7 @@ class Mailerdecorator
 			exit(0);
 		}
 	}
-	
+
 	public function sendadminmail($user)
 	{
 		try {
@@ -67,9 +65,9 @@ class Mailerdecorator
 			$this->mailer->setFrom("TGI", "contact@tonikgrupimage.com");
 			$this->mailer->addRecipient($user['first_name'].', '.$user['last_name'], $user['email']);
 			$this->mailer->fillSubject("Tonik Stagin admin");
-			$this->mailer->fillMessage($this->message);			
+			$this->mailer->fillMessage($this->message);
 			$this->mailer->addRecipient("Skander Jabouzi", "skander.jabouzi@tonikgroupimage.com");
-			
+
 			// now we send it!
 			$this->mailer->send();
 		} catch (Exception $e) {
