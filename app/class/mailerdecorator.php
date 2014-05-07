96c0204b2458ca346af1be58e7c3fdf2e359ee4d
Skander Jabouzi <jabouzi@gmail.com>
2014-05-07 10:44:17 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@651 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- app/class/mailerdecorator.php
+++ app/class/mailerdecorator.php
@@ -38,7 +38,7 @@ class Mailerdecorator
 		try {
 			$this->mailer = new Mailer();
 			$this->mailer->setFrom("TGI", "contact@tonikgrupimage.com");
-			$this->mailer->addRecipient($user['user_first_name'].', '.$user['user_last_name'], $user['user_email'];
+			$this->mailer->addRecipient($user['user_first_name'].', '.$user['user_last_name'], $user['user_email']);
 			$this->mailer->fillSubject("Tonik Stagin website");
 			$this->mailer->fillMessage($this->message);			
 			$this->mailer->addRecipient("Skander Jabouzi", "skander.jabouzi@tonikgroupimage.com");
@@ -56,7 +56,7 @@ class Mailerdecorator
 		try {
 			$this->mailer = new Mailer();
 			$this->mailer->setFrom("TGI", "contact@tonikgrupimage.com");
-			$this->mailer->addRecipient($user['first_name'].', '.$user['last_name'], $user['email'];
+			$this->mailer->addRecipient($user['first_name'].', '.$user['last_name'], $user['email']);
 			$this->mailer->fillSubject("Tonik Stagin admin");
 			$this->mailer->fillMessage($this->message);			
 			$this->mailer->addRecipient("Skander Jabouzi", "skander.jabouzi@tonikgroupimage.com");
