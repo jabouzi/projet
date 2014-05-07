76346989ed88a209463c5e0a5fe398d5c4924cb6
Skander Jabouzi <jabouzi@gmail.com>
2014-05-07 02:27:24 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@611 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- app/lib/xmlimportadapter.php
+++ app/lib/xmlimportadapter.php
@@ -14,7 +14,7 @@ class Xmlimportadapter
 		$racine = simplexml_load_file($file);
 		foreach ($racine->user_info as $key => $usr_info) 
 		{
-			$var_dump((array)$usr_info);
+			var_dump((array)$usr_info);
 		}
 
 	}
