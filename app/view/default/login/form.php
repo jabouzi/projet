0e40280cdb5e09e939dfe5aa36da2a1362935063
Skander Jabouzi <jabouzi@gmail.com>
2014-05-08 09:01:50 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@706 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- app/view/default/login/form.php
+++ app/view/default/login/form.php
@@ -4,6 +4,6 @@
 		<div class="row error"><label></label><?php echo display_message(); ?></div>
 		<div class="row"><label for="email"><?php echo lang('form.username'); ?>:</label><input type="text" name="email" id="email" value="<?php if (isset($email)) echo $email; ?>"/></div>
 		<div class="row"><label for="password"><?php echo lang('form.password'); ?>:</label><input type="password" name="password" id="password" value="<?php if (isset($password)) echo $password; ?>"/></div>
-		<div class="row"><label for="submit"> </label><input id="submit" type="submit" value="login" class="submitbutton" /></div>
+		<div class="row"><label for="submit"> </label><input id="submit" type="submit" value="<?php echo lang('title.login'); ?>" class="submitbutton" /></div>
 	</form>
 </div>
