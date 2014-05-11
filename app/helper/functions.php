3b035dbcdc337546289f85a524b901b76e836de0
Skander Jabouzi <jabouzi@gmail.com>
2014-05-11 17:06:27 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@807 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- app/helper/functions.php
+++ app/helper/functions.php
@@ -45,9 +45,9 @@ function compare_user_data($user1, $user2)
 }
 
 function compare_user_admin($user1, $user2)
-{	
+{
 	if (isempty($user1['password'])) $user2['password'] = '';
-	if (!isset($user1['admin'])) unset($user2['admin']);
-	if (!isset($user1['status'])) unset($user2['status']);
+	if (!isset($user1['admin'])) $user1['admin'] = 0;
+	if (!isset($user1['status'])) $user1['status'] = 0;
 	return array_merge(array_diff_assoc($user1, $user2), array_diff_assoc($user2, $user1));
 }
