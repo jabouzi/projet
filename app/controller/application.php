f2b1be7e9a3e92e9884106a3b8d3a6b0cdd998c3
Skander Jabouzi <jabouzi@gmail.com>
2014-05-11 17:00:57 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/projet/trunk@803 b4f2fc90-cf10-4682-8399-fbf5ccf6d971

--- app/controller/application.php
+++ app/controller/application.php
@@ -106,6 +106,7 @@ class Application extends Controller
 			{
 				$this->usermodel->update_user($_POST);
 				$user = $this->usermodel->get_user($_POST['user_name'])->__toArray();
+				var_dump($user);exit;
 				$this->sendemail($user, self::EDIT);
 				$_SESSION['message'] = lang('account.user.updated');
 			}
